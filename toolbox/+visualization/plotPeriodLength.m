78201d67e6db499897ec51dc6a90f7b841d9bd74
Gembu Maryu <gmaryu@umich.edu>
2025-08-23 12:26:50 -0400

    Updated plotRaster.m and plotPeriodLength.m

--- toolbox/+visualization/plotPeriodLength.m
+++ toolbox/+visualization/plotPeriodLength.m
@@ -1,18 +1,70 @@
-function ps = plotPeriodLength(peakMatrix, FramToMin)
+function [f, plMat] = plotPeriodLength(dataSet, varargin)
 
-p1 = peakMatrix(:,1:size(peakMatrix,2)-1);
-p2 = peakMatrix(:,2:size(peakMatrix,2));
-ps = (p2-p1)*FramToMin;
-
-figure();
-x = [];
-y = [];
-for c = 1:size(ps,2)
-    x = [x c*ones(1,size(peakMatrix,1))];
-    y = [y ps(:,c)'];
-end
-swarmchart(x,y);
-xlabel('Cycle Number');
-ylabel('Period length (min)');
+% plotPeriodLength - Swarm plot for cell cycle periods with cycle number
+%  
+% Usage:
+%   f = plotPeriodLength(dataSet);                 
+%   f = plotPeriodLength(dataSet, 'MarkerSize', 5, 'Color', 'r');
+%
+% Name-Value options:
+%   'Axes'        (axes)    : Target axes handle for plotting (default: gca)
+%   'MarkerSize'  (scalar)  : Marker size for scatter (default: 3)
+%   'Color'       (char/str): Color for markers/lines (default: 'k')
+   
+    visualization.plotFiguresPreamble;
+    
+    % ---- Parse inputs ----
+    p = inputParser;
+    addRequired(p, 'data', @(s)isstruct(s) && isfield(s,'cycle') && isfield(s,'info') && isfield(s,'FrameToMin'));
+    addParameter(p, 'Axes', [], @(ax) isempty(ax) || isa(ax,'matlab.graphics.axis.Axes'));
+    addParameter(p, 'MarkerSize', 8, @(x)isnumeric(x) && isscalar(x) && x>0);
+    addParameter(p, 'Color', 'k', @(c)ischar(c) || isstring(c) || (isnumeric(c) && numel(c)==3));
+    parse(p, dataSet, varargin{:});
+    
+    % ---- Target Figure Pannel ----
+    ax          = p.Results.Axes;
+    if isempty(ax), ax = gca; end
+    msize       = p.Results.MarkerSize;
+    colorSpec   = p.Results.Color;
+    
+    % ---- Data preprocessing ----
+    % add IGNORED column in case the table doesn't have the column (accept
+    % all droplets)
+    if ~ismember("IGNORED", dataSet.info.Properties.VariableNames)
+        disp("IGNORED is temporally added to dataSet.Info");
+        dataSet.info.IGNORED = zeros(size(dataSet.info,1),1);
+    end
+    
+    % Peak time collection
+    idxCont = dataSet.info(dataSet.info.IGNORED == 0, :);
+    maxCycle = max(dataSet.cycle.CYCLE_ID);
+    ptmCont = nan * zeros(size(idxCont, 1), maxCycle+1);
+    for i = 1:size(idxCont)
+        pid = idxCont.POS_ID(i);
+        did = idxCont.TRACK_ID(i);
+        pcycle = dataSet.cycle(bitand(dataSet.cycle.POS_ID == pid, dataSet.cycle.TRACK_ID == did), :);
+        peakTime = [pcycle.START_FRAME; pcycle.END_FRAME(end)]' * dataSet.FrameToMin;
+        ptmCont(i, 1:length(peakTime)) = peakTime;
+    end 
+    
+    % Calculate period length from the peak time matrix
+    plMat = diff(ptmCont, 1, 2);
+    
+    % ---- Draw a swarm plot ----
+    x=[];
+    y=[];
+    for c = 1:size(plMat,2)
+        x = [x c*ones(1,size(plMat,1))];
+        y = [y plMat(:,c)'];
+    end
+    f = figure();
+    swarmchart(x,y,msize,colorSpec);
+    xlabel('Cycle Number');
+    ylabel('Period length (min)');
+    set(ax, "XGrid","off","YGrid","on");
+    
+    % ---- Figure Properties for export ----
+    f.Units = 'centimeters';
+    f.Position = [2,2,maxCycle+1,5];
 
 end
--- toolbox/+visualization/plotRaster.m
+++ toolbox/+visualization/plotRaster.m
@@ -1,107 +1,125 @@
-function [props, peakMatrix] = plotRaster(dataSet, line_option)
+function [props, peakMatrix] = plotRaster(dataSet, varargin)
+% plotRaster - Raster plot with optional separator lines
+%
+% Usage:
+%   [props, peakMatrix] = plotRaster(dataSet);                          % without lines
+%   [props, peakMatrix] = plotRaster(dataSet, 'line_option', true);     % with separator lines
+%   [props, peakMatrix] = plotRaster(dataSet, 'line_option', true, ...
+%                                    'MarkerSize', 10, 'Color', 'k');
+%
+% Name-Value options:
+%   'line_option' (logical) : Draw separator lines for accumulated droplet counts per position (default: false)
+%   'Axes'        (axes)    : Target axes handle for plotting (default: gca)
+%   'MarkerSize'  (scalar)  : Marker size for scatter (default: 10)
+%   'lineColor'       (char/str): Color for lines (default: 'k')
 
-% return values;
-peakMatrix = [];
-vec_pos = [];
-vec_trackid = [];
-vec_maxCycles = [];
-vec_medianDiameter =[];
-%vec_fastestPreiods = [];
-%vec_slowestPreiods = [];
+    % ---- Parse inputs ----
+    p = inputParser;
+    addRequired(p, 'dataSet', @(s)isstruct(s) && isfield(s,'cycle') && isfield(s,'info') && isfield(s,'FrameToMin'));
+    addParameter(p, 'line_option', false, @(x)islogical(x) || isnumeric(x));
+    addParameter(p, 'Axes', [], @(ax) isempty(ax) || isa(ax,'matlab.graphics.axis.Axes'));
+    addParameter(p, 'MarkerSize', 5, @(x)isnumeric(x) && isscalar(x) && x>0);
+    addParameter(p, 'lineColor', 'k', @(c)ischar(c) || isstring(c) || (isnumeric(c) && numel(c)==3));
+    parse(p, dataSet, varargin{:});
 
+    line_option = logical(p.Results.line_option);
+    ax          = p.Results.Axes;
+    if isempty(ax), ax = gca; end
+    msize       = p.Results.MarkerSize;
+    colorSpec   = p.Results.Color;
 
-count = 0;                              % counting a number of droplets
-max_cyc = max(dataSet.cycle.CYCLE_ID);  % max cycle number in this condition
-total_peaks = dataSet.cycle;            % peaks information
-info = dataSet.info;                    % 
-unique_pos = unique(total_peaks.POS_ID);% a list of position of this condition
-count_vector =nan(size(unique_pos));    % number of droplet counts for each position
-
-
-for p = 1:length(unique_pos)
-    tmp_cycle_pos = total_peaks(total_peaks.POS_ID == unique_pos(p),:);
-    tmp_info_pos = info(info.POS_ID == unique_pos(p),:);
-    unique_droplets = unique(tmp_cycle_pos.TRACK_ID);
-    for d = 1:length(unique_droplets)
-        tmp_droplet = tmp_cycle_pos(tmp_cycle_pos.TRACK_ID == unique_droplets(d),:);
-        tmp_info_droplet = tmp_info_pos(tmp_info_pos.TRACK_ID == unique_droplets(d),:);
-        tmp_peaks = zeros(max_cyc+1,1)*NaN;
-        allpeaks = unique([tmp_droplet.START_FRAME; tmp_droplet.END_FRAME]);
-        tmp_peaks(1:height(allpeaks)) = allpeaks;
-        peakMatrix = [peakMatrix; tmp_peaks'];
-        vec_pos = [vec_pos, unique_pos(p)];
-        vec_trackid = [vec_trackid, unique_droplets(d)];
-        vec_maxCycles = [vec_maxCycles, tmp_info_droplet.CYCLE_NUMBER+1];
-        vec_medianDiameter = [vec_medianDiameter, tmp_info_droplet.ORIGINAL_MED_DIAMETER];
+    % add IGNORED column in case tables doesn't have the column name
+    % (accept all doplets info)
+    if ~ismember("IGNORED", dataSet.info.Properties.VariableNames)
+        disp("IGNORED is temporally added to dataSet.Info");
+        dataSet.info.IGNORED = zeros(size(dataSet.info,1),1);
     end
-    count = count + length(unique_droplets);
-    count_vector(p) = count;
-end
-props.POS_ID = vec_pos';
-props.TRACK_ID = vec_trackid';
-props.maxCycles = vec_maxCycles';
-props.medianDiameter = vec_medianDiameter';
-%{
-% all 
-if strcmp(mode,'all')
-    for p = 1:length(unique_pos)
-        tmp_pos = total_peaks(total_peaks.POS_ID == unique_pos(p),:);
-        unique_droplets = unique(tmp_pos.TRACK_ID);
-        for d = 1:length(unique_droplets)
-            tmp_droplet = tmp_pos(tmp_pos.TRACK_ID == unique_droplets(d),:);
-            tmp_peaks = zeros(max_cyc+1,1)*NaN;
+    if ~ismember("IGNORED", dataSet.cycle.Properties.VariableNames)
+        disp("IGNORED is temporally added to dataSet.cycle");
+        dataSet.cycle.IGNORED = zeros(size(dataSet.cycle,1),1);
+    end
+    
+
+    % ---- Initialize return values ----
+    peakMatrix = [];
+    vec_pos = [];
+    vec_trackid = [];
+    vec_maxCycles = [];
+    vec_medianDiameter = [];
+
+    count = 0;
+    max_cyc     = max(dataSet.cycle.CYCLE_ID);  % maximum cycle number in this condition
+    total_peaks = dataSet.cycle;                % peak information (table expected)
+    info        = dataSet.info;                 % additional information (table expected)
+    unique_pos  = unique(total_peaks.POS_ID);   % list of unique position IDs
+    count_vector = nan(size(unique_pos));       % cumulative droplet counts for each position (for separator lines)
+
+    % ---- Construct peak matrix ----
+    for ppos = 1:numel(unique_pos)
+        tmp_cycle_pos = total_peaks(bitand(total_peaks.POS_ID == unique_pos(ppos), total_peaks.IGNORED == 0), :);
+        tmp_info_pos  = info(info.POS_ID == unique_pos(ppos), :);
+        unique_droplets = unique(tmp_cycle_pos.TRACK_ID);
+
+        for d = 1:numel(unique_droplets)
+            tmp_droplet = tmp_cycle_pos(tmp_cycle_pos.TRACK_ID == unique_droplets(d), :);
+            tmp_info_droplet = tmp_info_pos(tmp_info_pos.TRACK_ID == unique_droplets(d), :);
+
+            % NaN vector with length = max_cyc+1
+            tmp_peaks = nan(max_cyc+1, 1);
+
+            % Collect unique start/end frames
             allpeaks = unique([tmp_droplet.START_FRAME; tmp_droplet.END_FRAME]);
-            tmp_peaks(1:height(allpeaks)) = allpeaks;
-            peakMatrix = [peakMatrix; tmp_peaks'];
-        end
-        count = count + length(unique_droplets);
-        count_vector(p) = count;
-    end
-elseif strcmp(mode,'nuc')
-    for p = 1:length(unique_pos)
-        tmp_pos = total_peaks(total_peaks.POS_ID == unique_pos(p),:);
-        unique_droplets = dataSet.info.TRACK_ID(bitand(dataSet.info.POS_ID == unique_pos(p), dataSet.info.SPERM_COUNT > 0));
-        for d = 1:length(unique_droplets)
-            tmp_droplet = tmp_pos(tmp_pos.TRACK_ID == unique_droplets(d),:);
-            tmp_peaks = zeros(max_cyc+1,1)*NaN;
-            allpeaks = unique([tmp_droplet.START_FRAME; tmp_droplet.END_FRAME]);
-            tmp_peaks(1:height(allpeaks)) = allpeaks;
-            peakMatrix = [peakMatrix; tmp_peaks'];
-        end
-        count = count + length(unique_droplets);
-        count_vector(p) = count;
-    end
-elseif strcmp(mode,'cyt')
-    for p = 1:length(unique_pos)
-        tmp_pos = total_peaks(total_peaks.POS_ID == unique_pos(p),:);
-        dataSet.info.SPERM_COUNT(isnan(dataSet.info.SPERM_COUNT)) = 0;
-        unique_droplets = dataSet.info.TRACK_ID(bitand(dataSet.info.POS_ID == unique_pos(p), dataSet.info.SPERM_COUNT == 0));
-        for d = 1:length(unique_droplets)
-            tmp_droplet = tmp_pos(tmp_pos.TRACK_ID == unique_droplets(d),:);
-            tmp_peaks = zeros(max_cyc+1,1)*NaN;
-            allpeaks = unique([tmp_droplet.START_FRAME; tmp_droplet.END_FRAME]);
-            tmp_peaks(1:height(allpeaks)) = allpeaks;
-            peakMatrix = [peakMatrix; tmp_peaks'];
-        end
-        count = count + length(unique_droplets);
-        count_vector(p) = count;
-    end
-else
-    disp('Invalid option: Choose all/nuc/cyt');
-end
-%}
+            nput = min(numel(allpeaks), max_cyc+1);
+            if nput > 0
+                tmp_peaks(1:nput) = allpeaks(1:nput);
+            end
 
-%figure(); 
-hold on
-for c = 1:size(peakMatrix,2)
-    tmp_c = peakMatrix(:,c);
-    scatter(tmp_c*dataSet.FrameToMin,1:size(peakMatrix,1),10,'o','filled');
-end
-ylim([1,size(peakMatrix,1)])
-if line_option
-    yline(count_vector, '--');
-end
-xlabel('Time (min)');
-hold off;
+            % Append droplet as a row in peakMatrix
+            peakMatrix = [peakMatrix; tmp_peaks.']; %#ok<AGROW>
 
+            % Store droplet attributes
+            vec_pos            = [vec_pos; unique_pos(ppos)]; %#ok<AGROW>
+            vec_trackid        = [vec_trackid; unique_droplets(d)]; %#ok<AGROW>
+            vec_maxCycles      = [vec_maxCycles; tmp_info_droplet.CYCLE_NUMBER + 1]; %#ok<AGROW>
+            vec_medianDiameter = [vec_medianDiameter; tmp_info_droplet.ORIGINAL_MED_DIAMETER]; %#ok<AGROW>
+        end
+
+        % Update droplet count for this position
+        count = count + numel(unique_droplets);
+        count_vector(ppos) = count;
+    end
+
+    % ---- Construct props output ----
+    props.POS_ID         = vec_pos;
+    props.TRACK_ID       = vec_trackid;
+    props.maxCycles      = vec_maxCycles;
+    props.medianDiameter = vec_medianDiameter;
+
+    % ---- Plot ----
+    axes(ax); hold(ax, 'on');
+
+    nRows = size(peakMatrix, 1);
+    nCols = size(peakMatrix, 2);
+
+    % Scatter plot for each column (cycle)
+    % NaNs will be ignored automatically by scatter
+    for c = 1:nCols
+        t = peakMatrix(:, c) * dataSet.FrameToMin;    % frame -> minutes
+        y = (1:nRows).';                              % droplet index
+        scatter(ax, t, y, msize, 'o', 'filled');
+    end
+    ylim(ax, [1, nRows]);
+    xlabel(ax, 'Time (min)');
+
+    % Draw separator lines if enabled
+    if line_option
+        yline(ax, count_vector, '--', 'Color', colorSpec);
+    end
+
+    % ---- Figure Properties for export ----
+    f = gcf;
+    f.Units = 'centimeters';
+    f.Position = [2,2,9,6];
+
+    hold(ax, 'off');
 end
